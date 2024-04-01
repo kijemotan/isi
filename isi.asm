62e8dc217299281c3cfd011c0e05a4c06e5455fe
the motan <kijemotan@gmail.com>
2024-04-01 12:24:35 +0200

    loading test message WITH COLOURS, ENCODED, FROM RAM!

--- isi.asm
+++ isi.asm
@@ -7,11 +7,11 @@
       ; (you can probably find it in
        ; #asm-dev lol)     - ss[motan]
 
-.org $080d
 .segment "STARTUP"
 .segment "INIT"
 .segment "ONCE"
 .segment "CODE"
+  jmp start
 
 ; VRAM MAP
 ; $00000-$04000 -> charset
@@ -43,24 +43,25 @@ PALETTE   = $1FA00  ; palette data until $1FBFF
 BANK    = $00
 CLCOUNT = $22       ; unused
 CURCLR  = $23
-COLOURS = $24       ; until $43
-L0START = $A000
-L0END   = $BFFF
-L1START = $C000
-L1END   = $DFFF
+CLRSEL  = $24
+COLOURS = $25       ; until $43
+INDEX_L = $45
+INDEX_H = $46
+TXSTART = $A000
 
 FONT: .literal "FONT.BIN"
 TEXT: .literal "TEXT.ISI"
 
 ; 8 colours:        #444    #FFF    #F44    #FF4    #4F4    #4FF    #44F    #F4F
 DEFAULTCLR: .byte $44,$04,$FF,$0F,$44,$0F,$F4,$0F,$F4,$04,$FF,$04,$4F,$04,$4F,$0F
-; ca65 is deciding to only store 6 of them. wtf.
- 
-  jmp start
+; ca65 is deciding to only store 6 of them in the list file. wtf.
+
+;                   ni  li  pona ala pona    cs  c3  T   cs  c4  E   c6  S   T   eof
+TESTMESSAGE: .byte $B1,$92,$C5, $62,$C5,$10,$F8,$F3,$54,$F8,$F4,$45,$F6,$53,$54,$FF
 
 start:
 
-  stp
+; stp
 
   stz V_CTRL
   lda #%01010001  ; VGA, layer 1, layer 0, sprites
@@ -95,77 +96,28 @@ start:
   ldy #>CHARSET
   jsr LOAD
 
-  lda #(1<<4 + ^L0_START)  ; address increment - 1, highest bit of address
+  lda #(1<<4 + ^L0_START)   ; address increment - 1, highest bit of address
   sta V_ADDRx_H
-  lda #>L0_START           ; middle byte of address
+  lda #>L0_START            ; middle byte of address
   sta V_ADDRx_M
-  lda #<L0_START           ; low byte of address
+  lda #<L0_START            ; low byte of address
   sta V_ADDRx_L
   
-  jsr L0_clearloop
-
-  ; test colours
-  lda #(1<<4 + ^L0_START)
-  sta V_ADDRx_H
-  lda #>L0_START
-  sta V_ADDRx_M
-  lda #(<L0_START + 1)
-  sta V_ADDRx_L
-
-  lda #$00
-  sta V_DATA0
   lda #$10
-  sta V_DATA0
-  lda #$00
-  sta V_DATA0
-  lda #$21
-  sta V_DATA0
-  lda #$00
-  sta V_DATA0
-  lda #$32
-  sta V_DATA0
-  lda #$00
-  sta V_DATA0
-  lda #$43
-  sta V_DATA0
-  lda #$00
-  sta V_DATA0
-  lda #$54
-  sta V_DATA0
-  lda #$00
-  sta V_DATA0
-  lda #$65
-  sta V_DATA0
-  lda #$00
-  sta V_DATA0
-  lda #$76
-  sta V_DATA0
-  lda #$00
-  sta V_DATA0
-  lda #$07
-  sta V_DATA0
-  
+  sta CURCLR
+  stz CLRSEL
 
-; lda #$DE
-; sta V_DATA0
-; lda #$01
-; sta V_DATA0
-; lda #$60
-; sta V_DATA0
-; lda #$01
-; sta V_DATA0
+  jsr L0_clearloop 
 
-;
+  ldx #0
+: lda TESTMESSAGE,x
+  sta TXSTART,x
+  cmp #$FF
+  beq :+
+  inx
+  bra :-
 
-; lda #$08  ; TEXT filename length
-; ldx #<TEXT
-; ldy #>TEXT
-; jsr SETNAM
-
-; lda #$02
-; ldx #<L0_START
-; ldy #>L0_START
-; jsr LOAD
+: jsr update
 
   rts
 
@@ -173,11 +125,11 @@ defaultpalette:
 
   lda #0
 : tax
-  sta DEFAULTCLR,x
-  lda COLOURS,x
+  lda DEFAULTCLR,x
+  sta COLOURS,x
   txa 
   ina
-  cmp $10
+  cmp #$10
   bne :-
 
 loadpalette:
@@ -195,41 +147,154 @@ loadpalette:
   sta V_DATA0
   txa 
   ina
-  cmp $10
+  cmp #$10
   bne :-
 
   rts
 
 L0_clearloop:
+
   lda V_ADDRx_M
   cmp #$60
-  beq L1_escape
+  beq L1_clearloop
   lda #$10      ; space
   sta V_DATA0
-  lda #$10      ; white on black
+  lda #$10      ; white on grey
   sta V_DATA0
+
 L0_checkaddr:
+
   lda V_ADDRx_L
   cmp #$A0      ; check if V_ADDRx_L=$A0
   bne L0_clearloop
   inc V_ADDRx_M
   stz V_ADDRx_L
   bne L0_clearloop
+  
 L1_clearloop:
+
   lda V_ADDRx_M
   cmp #$80
-  beq L1_escape
+  beq RAM_clearloop
   lda #$10      ; space
   sta V_DATA0
   lda #$10      ; white on transparent
   sta V_DATA0
+
 L1_checkaddr:
+
   lda V_ADDRx_L
   cmp #$A0      ; check if V_ADDRx_L=$A0
   bne L1_clearloop
   inc V_ADDRx_M
   stz V_ADDRx_L
   bne L1_clearloop
-L1_escape:
+
+RAM_clearloop:
+  
+; stp
+
+  lda #<TXSTART
+  sta INDEX_L
+  lda #>TXSTART
+  sta INDEX_H
+: lda #$10
+  sta (INDEX_L)
+  inc INDEX_L
+  lda INDEX_L
+  bne :+
+  inc INDEX_H
+; stp
+: lda INDEX_H
+  cmp #$C0
+  bcc :--
+
   rts
 
+;--- everything below here is unfinished and will not run
+
+nextindex:
+  
+  inc INDEX_L
+  bne checkchar
+  inc INDEX_H
+  bra checkchar
+
+update:
+  
+  stp
+
+  lda #(1<<4 + ^L0_START) ; set cursor to upper left
+  sta V_ADDRx_H
+  lda #>L0_START
+  sta V_ADDRx_M
+  lda #<L0_START
+  sta V_ADDRx_L 
+
+  lda #<TXSTART
+  sta INDEX_L
+  lda #>TXSTART
+  sta INDEX_H
+  
+checkchar:
+
+  lda (INDEX_L)
+  cmp #$FF      ; character >= $FF? (EOF)
+  bne :+        ; if not, branch
+  rts
+: cmp #$EF      ; character >= $EF? (visible symbol)
+  bcc :+        ; if not, branch
+  jsr checkmeta
+  bra nextindex
+
+: sta V_DATA0
+  lda CURCLR
+  sta V_DATA0
+
+  bra nextindex
+
+checkmeta:
+
+  cmp #$F8      ; character >= $F8?
+  bcs :+        ; if yes, branch
+  jsr setcolour
+  rts
+
+: bne :+        ; if not equal, branch
+  pha
+  lda CLRSEL
+  eor #1
+  sta CLRSEL
+  pla
+
+: rts
+
+setcolour:
+
+  and #$0F
+  ldx CLRSEL
+  cpx #1      ; CRLSEL = 1?
+  bne :+      ; if not, branch
+
+  asl a       ; set bg colour
+  asl a
+  asl a
+  asl a       ; eg. $F5 -> $50
+  pha
+  lda CURCLR
+  and #$0F
+  sta CURCLR
+  pla
+  ora CURCLR
+  sta CURCLR
+  rts
+
+: pha         ; set fg colour
+  lda CURCLR
+  and #$F0
+  sta CURCLR
+  pla
+  ora CURCLR
+  sta CURCLR
+
+  rts
